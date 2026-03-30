b6fe37b1dbea64ec3a040245c82cd17188998986
Mitchell Goff <mitchellgoffpc@gmail.com>
2026-03-29 21:37:23 -0700

    Stricter allow_origins

--- underfit_api/main.py
+++ underfit_api/main.py
@@ -79,7 +79,7 @@ if config.frontend_url:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
